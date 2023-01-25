0cca48f152e1a26584644718d2ce161ba79f9236
rebaz36 <rebaz415@gmail.com>
2023-01-25 11:11:01 +0300

    revert commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
-  resources :posts, only: [:new, :create]
-  resources :comments, only: [:new, :create]
-  resources :likes, only: [:new, :create]
-
   root "users#index"
 end
